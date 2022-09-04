5bccea5ad2e1c4732f679ab8ad6be4f82ede0b28
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-04 22:15:29 +0300

    Task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const markup = ingredients
+  .map((ingredient) => `<li class="item">${ingredient}</li>`)
+  .join("");
+
+ingredientsRef.insertAdjacentHTML("afterbegin", markup);
+
+console.log(ingredientsRef);
