10ff6b1d00328f4d1c3cffd917b4855e8ea3c49c
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-11 14:27:00 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+
+const btnChangeColorRef = document.querySelector(".change-color");
+
+const colorValueRef = document.querySelector(".color");
+
+btnChangeColorRef.addEventListener("click", () => {
+  const backgroundColor = getRandomHexColor();
+
+  body.style.backgroundColor = backgroundColor;
+  colorValueRef.innerHTML = backgroundColor;
+});
