4e71777e8b802052ab2ba24edb7e7d095185afb9
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-11 18:34:10 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,56 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let amount = 0;
+let size = 20;
+
+const controlsRef = document.querySelector("#controls");
+const createBtnRef = document.querySelector("button[data-create]");
+const destroyBtnRef = document.querySelector("button[data-destroy]");
+const boxesRef = document.querySelector("#boxes");
+
+boxesRef.style.display = "flex";
+boxesRef.style.flexWrap = "wrap";
+boxesRef.style.flexDirection = "column-reverse";
+
+function boxGetBigger(amount) {
+  for (let i = 0; i < amount; i++) {
+    size += 10;
+    return size;
+  }
+}
+
+controlsRef.addEventListener("input", (event) => {
+  amount = event.target.value;
+});
+
+createBtnRef.addEventListener("click", createBoxes());
+
+destroyBtnRef.addEventListener("click", () => {
+  destroyBoxes();
+  size = 20;
+});
+
+createBtnRef.addEventListener("click", () => {
+  createBoxes(amount);
+});
+
+function createBoxes(amount) {
+  if (amount >= 1) {
+    for (let i = 0; i < amount; i++) {
+      boxesRef.insertAdjacentHTML("afterbegin", "<div class=box></div>");
+      const boxRef = document.querySelector(".box");
+      boxRef.style.width = `${boxGetBigger(amount)}px`;
+      boxRef.style.height = boxRef.style.width;
+      boxRef.style.margin = "5px";
+      boxRef.style.backgroundColor = getRandomHexColor();
+    }
+  }
+}
+
+function destroyBoxes() {
+  return (boxesRef.innerHTML = "");
 }
