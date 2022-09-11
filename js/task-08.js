41dce3a41b6065e0fafddf1a93cae433d42954f7
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-11 12:32:22 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginFormRef = document.querySelector(".login-form");
 
+loginFormRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.target;
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены!");
+  }
+  const User = { email: email.value, password: password.value };
+
+  console.log(User);
+
+  event.target.reset();
+});
