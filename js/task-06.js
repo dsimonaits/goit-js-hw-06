afa64b21a31b4540d7731bf01ac75fe0ff6372db
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-13 22:48:45 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -13,16 +13,4 @@ const changeClass = (event) => {
   }
 };
 
-// const changeClass = (event) => {
-//   if (
-//     event.target.value.length >= validationInputRef.getAttribute("data-length")
-//   ) {
-//     validationInputRef.classList.remove("invalid");
-//     validationInputRef.classList.add("valid");
-//   } else {
-//     validationInputRef.classList.remove("valid");
-//     validationInputRef.classList.add("invalid");
-//   }
-// };
-
 validationInputRef.addEventListener("blur", changeClass);
