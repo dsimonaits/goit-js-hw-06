0b9ea007df431c416a9d635faee43e07a7f01906
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-13 20:29:34 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,47 +9,11 @@ const ingredients = [
 
 const ingredientsRef = document.querySelector("#ingredients");
 
-const ingredient1 = document.createElement("li");
-ingredient1.textContent = ingredients[0];
-ingredient1.classList.add("item");
+ingredients.map((ingredient) => {
+  const ingredientMarkup = document.createElement("li");
+  ingredientMarkup.textContent = ingredient;
+  ingredientMarkup.classList.add("item");
+  ingredientsRef.append(ingredientMarkup);
+});
 
-// console.log(ingredient1);
-
-const ingredient2 = document.createElement("li");
-ingredient2.textContent = ingredients[1];
-ingredient2.classList.add("item");
-
-// console.log(ingredient2);
-
-const ingredient3 = document.createElement("li");
-ingredient3.textContent = ingredients[2];
-ingredient3.classList.add("item");
-
-// console.log(ingredient3);
-
-const ingredient4 = document.createElement("li");
-ingredient4.textContent = ingredients[3];
-ingredient4.classList.add("item");
-
-// console.log(ingredient4);
-
-const ingredient5 = document.createElement("li");
-ingredient5.textContent = ingredients[4];
-ingredient5.classList.add("item");
-
-// console.log(ingredient5);
-
-const ingredient6 = document.createElement("li");
-ingredient6.textContent = ingredients[5];
-ingredient6.classList.add("item");
-
-// console.log(ingredient6);
-
-ingredientsRef.append(
-  ingredient1,
-  ingredient2,
-  ingredient3,
-  ingredient4,
-  ingredient5,
-  ingredient6
-);
+console.log(ingredientsRef);
